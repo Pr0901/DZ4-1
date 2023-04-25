96b6b35a79007b5b74e8ded4909c29ce23daa212
Sofya <sofa33158@gmail.com>
2023-04-25 13:04:05 +0300

    Initial committ

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/wi5vad36bfg2ginv/branch/main?svg=true)](https://ci.appveyor.com/project/Pr0901/dz4-1/branch/main)
---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import java.lang.module.Configuration;
+
+public class CardDeliveryTest {
+
+    @Test
+    void successfulAuthorization () {
+        Configuration.holdBrowserOpen = true;
+        open(http://localhost:9999/);
+    }
+}
